a6a54d54fe8d8cc26c1b3667110d78eea9276805
kaito_ohishi <kaito990626@yahoo.co.jp>
2020-05-20 11:27:04 +0900

    change_references_like_comment
    LikeテーブルとCommentテーブルのidカラムをreferences型に変更
    及び外部キー制約を追加した。

--- db/migrate/20200519001917_create_likes.rb
+++ db/migrate/20200519001917_create_likes.rb
@@ -1,8 +1,8 @@
 class CreateLikes < ActiveRecord::Migration[5.2]
   def change
     create_table :likes do |t|
-      t.integer :tweet_id, null: false
-      t.integer :user_id, null: false
+      t.references :tweet, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
