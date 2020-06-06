ca0dcef1e0eb456908a74aa90ba058610ce49e22
kaito_ohishi <kaito990626@yahoo.co.jp>
2020-06-07 08:08:14 +0900

    add_likes_count&devise_error_messages

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,7 +2,7 @@ class Like < ApplicationRecord
 # likes-------------------------------------------------------------------
   validates :tweet_id, uniqueness: { scope: :user_id }
 # tweets-------------------------------------------------------------------
-  belongs_to :tweet
+  belongs_to :tweet, counter_cache: :likes_count
 # users-------------------------------------------------------------------
   belongs_to :user
   
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -43,9 +43,21 @@ describe User do
     end
 
     it "重複したemailが存在する場合登録できないこと" do
-      user = build(:user, email: "guest@user")
+      user = create(:user)
+      another_user = build(:user, email: user.email)
+      another_user.valid?
+      expect(another_user.errors[:email]).to include("はすでに存在します")
+    end
+
+    it "passwordが6文字以上であれば登録できること" do
+      user = build(:user, password: "123456")
       user.valid?
-      expect(user.errors[:email]).to include("はすでに存在します")
+    end
+
+    it " passwordが5文字以下であれば登録できないこと " do
+      user = build(:user, password: "00000", password_confirmation: "00000")
+      user.valid?
+      expect(user.errors[:password]).to include("は6文字以上で入力してください")
     end
 
   end
