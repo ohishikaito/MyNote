ce98036bee7eb59068511e5f8862c8a6c3149530
kaito_ohishi <kaito990626@yahoo.o.jp+ohishikaito@users.noreply.github.com>
2020-06-28 12:52:53 +0900

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,4 +2,6 @@
 server '3.113.216.56', user: 'kaito', roles: %w{app db web}
 
 #デプロイするサーバーにsshログインする鍵の情報を記述
-set :ssh_options, keys: '~/.ssh/mynote_key_rsa'
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: '~/.ssh/mynote_key_rsa',
+                  forward_agent: true
