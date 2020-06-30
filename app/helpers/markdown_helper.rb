15ef0823463864f54784a40b8fbeaae193195c90
kaito_ohishi <kaito990626@yahoo.o.jp+ohishikaito@users.noreply.github.com>
2020-06-30 11:35:38 +0900

    [fix]モーダルのレスポンシブ化

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -2,14 +2,25 @@ module MarkdownHelper
   def markdown(text)
     unless @markdown
       options = {
-        hard_wrap: true
+        hard_wrap: true,
+
+        filter_html: true,
+        autolink: true,
+        space_after_headers: true,
+        no_intra_emphasis: true,
+        fenced_code_blocks: true,
+        tables: true,
+        xhtml: true,
+        lax_html_blocks: true,
+        strikethrough: true
+
       }
       extensions = {
-        no_intra_emphasis: true,
-        tables: true,
-        fenced_code_blocks: true,
-        autolink: true,
-        quote: true
+        # no_intra_emphasis: true,
+        # tables: true,
+        # fenced_code_blocks: true,
+        # autolink: true,
+        # quote: true
       }
       renderer = Redcarpet::Render::HTML.new(options)
       @markdown = Redcarpet::Markdown.new(renderer, extensions)
