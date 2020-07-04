7bf7743f48dc24e33ee0c99f20becd5526d9022a
kaito_ohishi <kaito990626@yahoo.o.jp+ohishikaito@users.noreply.github.com>
2020-07-05 08:53:44 +0900

    結合テスト

--- Gemfile
+++ Gemfile
@@ -51,6 +51,7 @@ group :development, :test do
   gem 'rspec-rails'
   gem 'rspec_junit_formatter'
   gem 'rails-controller-testing'
+  gem 'capybara', '>= 2.15'
 end
 
 group :production do
@@ -71,7 +72,6 @@ end
 
 group :test do
   # Adds support for Capybara system testing and selenium driver
-  gem 'capybara', '>= 2.15'
   gem 'selenium-webdriver'
   # Easy installation and use of chromedriver to run system tests with Chrome
   gem 'chromedriver-helper'
---
+++ spec/features/tweet_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+feature 'tweet', type: :feature do
+  let(:user) { create(:user) }
+
+  scenario 'ユーザー情報が更新されていること' do
+    # ログイン前には投稿ボタンがない
+    visit root_path
+    expect(page).to have_no_content("投稿")
+
+    # ログイン処理
+    visit new_user_session_path
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password', with: user.password
+    find(('input[value="ログインする"]')).click
+    expect(current_path).to eq root_path
+    expect(page).to have_content("投稿")
+
+    # 投稿処理
+    expect {
+      click_link("投稿")
+      expect(current_path).to eq new_tweet_path
+      fill_in 'tweet_title', with: "結合テスト"
+      fill_in 'md-textarea', with: "結合テスト"
+      find('input[type="submit"]').click
+    }.to change(Tweet, :count).by(1)
+  end
+
+end
