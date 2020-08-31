e5b485164a72a852e19cf09d691ec9ab3be5114b
kaito_ohishi <kaito990626@yahoo.o.jp+ohishikaito@users.noreply.github.com>
2020-08-31 15:37:31 +0900

    minitestフォルダを削除

--- test/controllers/tweets_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class TweetsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get tweets_index_url
-    assert_response :success
-  end
-end
