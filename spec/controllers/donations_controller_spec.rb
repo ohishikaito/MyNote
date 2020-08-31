b6f279b6ba59bd11cb4bb9b9c82ad162a681de4b
kaito_ohishi <kaito990626@yahoo.o.jp+ohishikaito@users.noreply.github.com>
2020-08-31 15:53:20 +0900

    add: 説明文を追加

---
+++ spec/controllers/donations_controller_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe 'Donations', type: :request do
+  describe 'GET #index' do
+    # 現在実装中
+  end
+end
---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe 'Users', type: :request do
+  describe 'GET #index' do
+    # 現在実装中
+  end
+end
