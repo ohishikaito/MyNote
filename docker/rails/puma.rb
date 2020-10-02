4c4e086e86470ec4c435490517c9c5097ab9c049
kaito_ohishi <kaito990626@yahoo.o.jp+ohishikaito@users.noreply.github.com>
2020-10-02 14:05:14 +0900

    rubocop -aによる修正

--- docker/rails/puma.rb
+++ docker/rails/puma.rb
@@ -4,5 +4,5 @@ port        ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RAILS_ENV") { "development" }
 plugin :tmp_restart
 
-app_root = File.expand_path("../..", __FILE__)
+app_root = File.expand_path('..', __dir__)
 bind "unix://#{app_root}/tmp/sockets/puma.sock"
