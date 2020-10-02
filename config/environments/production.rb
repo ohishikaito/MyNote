34664353c96fa37d9d7171c89a30947a48c3e80d
kaito_ohishi <kaito990626@yahoo.o.jp+ohishikaito@users.noreply.github.com>
2020-10-02 18:02:52 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,11 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # trueだと本番環境でもエラーを表示させる
+  # config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true
+  config.action_controller.perform_caching = true
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -10,9 +15,6 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
-  # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
