60d7d4b173aa8870cce2b7e3d4119ef2bdf4a201
kaito_ohishi <kaito990626@yahoo.o.jp+ohishikaito@users.noreply.github.com>
2020-10-03 09:08:06 +0900

    docker上でcrdコマンド実行可能

--- config/puma.rb
+++ config/puma.rb
@@ -1,12 +1,3 @@
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
-threads threads_count, threads_count
-port        ENV.fetch("PORT") { 3000 }
-environment ENV.fetch("RAILS_ENV") { "development" }
-plugin :tmp_restart
-
-app_root = File.expand_path("../..", __FILE__)
-bind "unix://#{app_root}/tmp/sockets/puma.sock"
-
 # 初期状態 --------------------------
 # threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 # threads threads_count, threads_count
@@ -14,3 +5,10 @@ bind "unix://#{app_root}/tmp/sockets/puma.sock"
 # environment ENV.fetch("RAILS_ENV") { "development" }
 # pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # plugin :tmp_restart
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
+threads threads_count, threads_count
+port        ENV.fetch("PORT") { 3000 }
+environment ENV.fetch("RAILS_ENV") { "development" }
+plugin :tmp_restart
+app_root = File.expand_path("../..", __FILE__)
+bind "unix://#{app_root}/tmp/sockets/puma.sock"
