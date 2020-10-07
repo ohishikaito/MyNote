3851ab114f43d1641dd90d64af08d49ecf52bbd6
kaito_ohishi <kaito990626@yahoo.o.jp+ohishikaito@users.noreply.github.com>
2020-10-07 19:13:54 +0900

    unicornを直デプロイに変更

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,18 +1,28 @@
-# ファイルが無いのでconfigフォルダにunicorn.rbを新規作成します
+#サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
+app_path = File.expand_path('../../', __FILE__)
 
-app_path = File.expand_path('../../../', __FILE__)
-# 上記からEC2からunicornを再起動可能にするために修正(6/29) 問題があれば上記に戻す
-# app_path = File.expand_path('/var/www/MyNote')
-
+#アプリケーションサーバの性能を決定する
 worker_processes 1
 
-working_directory "#{app_path}/current"
-pid "#{app_path}/shared/tmp/pids/unicorn.pid"
-listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
-stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
-stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
+#アプリケーションの設置されているディレクトリを指定
+working_directory app_path
 
+#Unicornの起動に必要なファイルの設置場所を指定
+pid "#{app_path}/tmp/pids/unicorn.pid"
+
+#ポート番号を指定
+listen 3000
+
+#エラーのログを記録するファイルを指定
+stderr_path "#{app_path}/log/unicorn.stderr.log"
+
+#通常のログを記録するファイルを指定
+stdout_path "#{app_path}/log/unicorn.stdout.log"
+
+#Railsアプリケーションの応答を待つ上限時間を設定
 timeout 60
+
+#以下は応用的な設定なので説明は割愛
 
 preload_app true
 GC.respond_to?(:copy_on_write_friendly=) && GC.copy_on_write_friendly = true
@@ -43,3 +53,49 @@ end
 after_fork do |_server, _worker|
   defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
 end
+
+# # ファイルが無いのでconfigフォルダにunicorn.rbを新規作成します
+
+# app_path = File.expand_path('../../../', __FILE__)
+# # 上記からEC2からunicornを再起動可能にするために修正(6/29) 問題があれば上記に戻す
+# # app_path = File.expand_path('/var/www/MyNote')
+
+# worker_processes 1
+
+# working_directory "#{app_path}/current"
+# pid "#{app_path}/shared/tmp/pids/unicorn.pid"
+# listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
+# stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
+# stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
+
+# timeout 60
+
+# preload_app true
+# GC.respond_to?(:copy_on_write_friendly=) && GC.copy_on_write_friendly = true
+
+# check_client_connection false
+
+# run_once = true
+
+# before_fork do |server, worker|
+#   defined?(ActiveRecord::Base) &&
+#     ActiveRecord::Base.connection.disconnect!
+
+#   if run_once
+#     run_once = false # prevent from firing again
+#   end
+
+#   old_pid = "#{server.config[:pid]}.oldbin"
+#   if File.exist?(old_pid) && server.pid != old_pid
+#     begin
+#       sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+#       Process.kill(sig, File.read(old_pid).to_i)
+#     rescue Errno::ENOENT, Errno::ESRCH => e
+#       logger.error e
+#     end
+#   end
+# end
+
+# after_fork do |_server, _worker|
+#   defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
+# end
