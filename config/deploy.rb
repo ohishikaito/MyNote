1766062d9d287b51e87aa27c6d930da895d40b71
kaito_ohishi <kaito990626@yahoo.o.jp+ohishikaito@users.noreply.github.com>
2020-10-07 20:46:47 +0900

    cap仕様

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,6 @@
-# Gemfile.lockを見てcapistranoのバージョンを入れる
-lock "~> 3.14.1"
-
-# set :deploy_to, '/var/www/MyNote'
+# config valid only for current version of Capistrano
+# capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
+lock "3.14.1"
 
 # 自身のアプリ名、リポジトリ名を記述
 set :application, 'MyNote'
@@ -15,38 +14,72 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
 
-# chat-spaceで使ったpemを指定、どの公開鍵を利用してデプロイするか
+# どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['~/.ssh/kaitoaws.pem']
 
-# どの公開鍵を利用してデプロイするか
+# プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
 # Unicornの設定ファイルの場所
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
-
-set :linked_files, %w{ config/master.key }
-# master.key用のシンボリックリンクを追加
-# set :linked_files, %w[config/master.key]
 
 # デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
-    invoke 'unicorn:stop'
-    invoke 'unicorn:start'
+    invoke 'unicorn:restart'
   end
-
-  # desc 'upload master.key'
-  # task :upload do
-  #   on roles(:app) do |host|
-  #     if test "[ ! -d #{shared_path}/config ]"
-  #       execute "mkdir -p #{shared_path}/config"
-  #     end
-  #     # circleCIでマスターキーを取得するためコメントアウト
-  #     upload!('config/master.key', "#{shared_path}/config/master.key")
-  #   end
-  # end
-  # before :starting, 'deploy:upload'
-  # after :finishing, 'deploy:cleanup'
 end
+
+# # Gemfile.lockを見てcapistranoのバージョンを入れる
+# lock "3.14.1"
+
+# # 自身のアプリ名、リポジトリ名を記述
+# set :application, 'MyNote'
+
+# # どのリポジトリからアプリをpullするかを指定する
+# set :repo_url,  'git@github.com:ohishikaito/MyNote.git'
+
+# # バージョンが変わっても共通で参照するディレクトリを指定
+# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+# set :rbenv_type, :user
+# set :rbenv_ruby, '2.5.1'
+
+# # chat-spaceで使ったpemを指定、どの公開鍵を利用してデプロイするか
+# set :ssh_options, auth_methods: ['publickey'],
+#                   keys: ['~/.ssh/kaitoaws.pem']
+
+# # どの公開鍵を利用してデプロイするか
+# set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+# # Unicornの設定ファイルの場所
+# set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+# set :keep_releases, 5
+
+# set :linked_files, %w{ config/master.key }
+# # master.key用のシンボリックリンクを追加
+# # set :linked_files, %w[config/master.key]
+
+# # デプロイ処理が終わった後、Unicornを再起動するための記述
+# after 'deploy:publishing', 'deploy:restart'
+# namespace :deploy do
+#   task :restart do
+#     invoke 'unicorn:stop'
+#     invoke 'unicorn:start'
+#   end
+
+#   # desc 'upload master.key'
+#   # task :upload do
+#   #   on roles(:app) do |host|
+#   #     if test "[ ! -d #{shared_path}/config ]"
+#   #       execute "mkdir -p #{shared_path}/config"
+#   #     end
+#   #     # circleCIでマスターキーを取得するためコメントアウト
+#   #     upload!('config/master.key', "#{shared_path}/config/master.key")
+#   #   end
+#   # end
+#   # before :starting, 'deploy:upload'
+#   # after :finishing, 'deploy:cleanup'
+# end
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,23 +1,15 @@
-#サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
-app_path = File.expand_path('../../', __FILE__)
+# ../が一つ増えている
+app_path = File.expand_path('../../../', __FILE__)
 
-#アプリケーションサーバの性能を決定する
 worker_processes 1
+# currentを指定
+working_directory "#{app_path}/current"
 
-#アプリケーションの設置されているディレクトリを指定
-working_directory app_path
-
-#Unicornの起動に必要なファイルの設置場所を指定
-pid "#{app_path}/tmp/pids/unicorn.pid"
-
-#ポート番号を指定
-listen "#{app_path}/tmp/sockets/unicorn.sock"
-
-#エラーのログを記録するファイルを指定
-stderr_path "#{app_path}/log/unicorn.stderr.log"
-
-#通常のログを記録するファイルを指定
-stdout_path "#{app_path}/log/unicorn.stdout.log"
+# それぞれ、sharedの中を参照するよう変更
+listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"
+stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
+stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 
 #Railsアプリケーションの応答を待つ上限時間を設定
 timeout 60
