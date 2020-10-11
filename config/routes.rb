c7672b28e8cc9b1400d599ee4f28d62b2ba4522f
kaito_ohishi <kaito990626@yahoo.o.jp+ohishikaito@users.noreply.github.com>
2020-10-11 18:06:55 +0900

    コメントアウトを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  # devise_for :users, :controllers => {
-  #   sessions: 'users/sessions'
-  # }
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
